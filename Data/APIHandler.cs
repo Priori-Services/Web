8d9a170779955f8fd034ff896f101c6d106d9b8d
Tulili Blossom <tulilirockz.pub+git@gmail.com>
2023-04-24 01:58:07 +0000

    feat: blog consegue ler info e apihandler

---
+++ Data/APIHandler.cs
@@ -0,0 +1,37 @@
+using System.Text;
+using System.Text.Json;
+
+namespace PRIORI_SERVICES_WEB.Data.API;
+
+public static class APIHandler
+{
+    public static async Task<T?> FetchAbstractJsonObjectAsync<T>(string target_url, string? api_endpoint = null)
+    {
+        if (api_endpoint == null)
+            api_endpoint = DefaultConfig.API_ENDPOINT;
+
+        string? response = await new HttpClient().GetStringAsync($"http://{api_endpoint}/{target_url}");
+
+        MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(response));
+
+        return await JsonSerializer.DeserializeAsync<T>(stream);
+    }
+
+    public static async Task<T> FetchOrFallbackAsync<T>(string target_url, T fallback_object)
+    {
+        T? initial_object;
+        try
+        {
+            initial_object = await FetchAbstractJsonObjectAsync<T>(target_url);
+        }
+        catch (HttpRequestException)
+        {
+            initial_object = fallback_object;
+        }
+        if (initial_object == null)
+        {
+            return fallback_object;
+        }
+        return initial_object;
+    }
+}
