435978dfafede4969a0ff4a5f78000967eee8a3d
Tulili Blossom <tulilirockz.pub+git@gmail.com>
2023-04-27 00:35:56 -0300

    feat: DBO de cliente para comunicação com API

---
+++ Data/ClienteDBO.cs
@@ -0,0 +1,11 @@
+namespace PRIORI_SERVICES_WEB.Data.Model;
+public class ClienteDBO
+{
+    public int? id_tipoinvestidor { get; set; }
+    public int? id_consultor { get; set; }
+    public string? endereco { get; set; } = null;
+    public string? nome { get; set; }
+    public string? cpf { get; set; }
+    public string? email { get; set; }
+    public string? telefone { get; set; }
+}
