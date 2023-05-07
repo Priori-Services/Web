89bc9d46cbbbb985eb439cc5a5cc962b4ea019f8
sp-gu <sp_gu@outlook.com>
2023-05-07 00:14:19 -0300

    feat & refactor: criação invEfet. e design invDisp

--- Data/InvestimentoDBO.cs
+++ Data/InvestimentoDBO.cs
@@ -13,3 +13,16 @@ public class Investimento
     public Decimal valor_minimo { get; set; }
     public Decimal tempo_minimo { get; set; }
 }
+
+public class Carteira
+{
+    public int id_efetuacao { get; set; }
+    public int id_cliente_carteira { get; set; }
+    public int id_investimento { get; set; }
+    public string? status { get; set; } = String.Empty;
+    public Decimal rentabilidade_fixa { get; set; }
+    public Decimal rentabilidade_variavel { get; set; }
+    public Decimal valor_aplicado { get; set; }
+    public DateTime data_encerramento { get; set; }
+    public DateTime data_efetuacao { get; set; }
+}
