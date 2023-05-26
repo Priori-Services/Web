dd559c152256c6c3a1a3fe5d48ef859ee7d93697
Tulili Blossom <tulilirockz.pub+git@gmail.com>
2023-05-26 20:20:33 -0300

    feat: cores mais escuras do background para visibilidade
    Co-authored-by: sp-gu <sp-gu@users.noreply.github.com>

---
+++ Data/Types/BackgroundGradients.cs
@@ -0,0 +1,7 @@
+namespace PRIORI_SERVICES_WEB.Data.Types;
+
+public static class BackgroundGradients
+{
+    public const string BackgroundPrincipal = "background-image: linear-gradient(75deg, rgb(74, 5, 77), rgb(50, 0, 73))";
+    public const string BackgroundHighlight = "background-image: linear-gradient(75deg, rgb(155, 139, 224), rgb(45, 1, 204))";
+}
