3cc1355b98daa558616e36fd927e54821d716a76
Tulili <tulilirockz.pub+git@gmail.com>
2023-06-10 09:41:37 -0300

    fix: erros de variaveis de ambiente SMTP

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Blazored.Toast;
 using Blazored.Modal;
 using Blazored.LocalStorage;
 using PRIORI_SERVICES_WEB.Handler.Services;
+using PRIORI_SERVICES_WEB.Data.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,7 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredToast();
 builder.Services.AddSingleton<HttpClient>();
 builder.Services.AddScoped<IAsyncService, AsyncService>();
-builder.Services.AddScoped<ISMTPService, SMTPService>();
+builder.Services.AddScoped<ISMTPService, SMTPService>().AddOptions<SMTPConfiguration>().BindConfiguration(SMTPConfiguration.DefaultSectionName);
 
 var app = builder.Build();
 
