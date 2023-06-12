cbb9c708fdb0c9c3bb21bafd243743515dba8378
Tulili <tulilirockz.pub+git@gmail.com>
2023-06-12 20:18:30 -0300

    fix: contratação adiciona e remove pontuação corretamente

--- Data/SimulacaoID.cs
+++ Data/SimulacaoID.cs
@@ -6,4 +6,5 @@ public static class SimulacaoID
     public static int id_blog { get; set; }
     public static PostBlog? PostMain { get; set; }
     public static CategoriaBlog? CategoriaMain { get; set; }
+    public static int? IsUserConsultor {get;set; } = null;
 }
