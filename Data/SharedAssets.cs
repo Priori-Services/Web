42d475d123d09fddcccf4548e6a3f27f3271796a
Tulili <tulilirockz.pub+git@gmail.com>
2023-06-13 20:37:50 -0300

    refactor: mudar o nome de SimulacaoID para SharedAssets

--- Data/SharedAssets.cs
+++ Data/SharedAssets.cs
@@ -1,10 +1,10 @@
 namespace PRIORI_SERVICES_WEB.Data.Model;
 
-public static class SimulacaoID
+public static class SharedAssets
 {
     public static int id_selecionado { get; set; }
     public static int id_blog { get; set; }
     public static PostBlog? PostMain { get; set; }
     public static CategoriaBlog? CategoriaMain { get; set; }
-    public static int? IsUserConsultor {get;set; } = null;
+    public static int? IsUserConsultor { get; set; } = null;
 }
